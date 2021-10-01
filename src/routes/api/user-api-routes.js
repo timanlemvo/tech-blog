3c15bcd04c06f90096bfbd3db09813a177a539e3
timanlemvo <timanlemvo@gmail.com>
2021-09-30 22:55:51 -0700

    user creastion and get routes for users id

--- src/routes/api/user-api-routes.js
+++ src/routes/api/user-api-routes.js
@@ -18,13 +18,58 @@ router.get('/', async (req, res) => {
 });
 
 // Get a specific user
-router.get('/:userId', (req, res) => {
-  res.json({message: 'Get a specific user'});
+router.get('/:userId', async (req, res) => {
+  try {
+    const user = await db.User.findOne({
+      attributes: {
+        exclude: ['password']
+      },
+      where: {
+        id: req.params.userId
+      },
+    });
+
+    if (!user) {
+      res.status(404).json({message: 'User not found'});
+      return;
+    }
+
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // Logs a user in
-router.post('/login', (req, res) => {
-  res.json({message: 'Logs a user in'});
+router.post('/login', async (req, res) => {
+  try {
+    const user = await db.User.findOne({
+      attributes: {
+        exclude: ['password']
+      },
+      where: {
+        username: req.body.username,
+      },
+    })
+
+    if(!user) {
+      res.status(400).json({message: 'User not found'});
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.username = user.username;
+      req.session.loggedIn = true;
+    });
+
+    res.json({ user: user, message: 'User is not logged in.'});
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // Creates a new user
@@ -48,7 +93,6 @@ router.post('/', async (req, res) => {
     console.log(err);
     res.status(500).json(err);
   }
-  res.json({message: 'Creates a new user'});
 });
 
 // Deletes a user
